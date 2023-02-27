764a88d438d0a50d369b7f38cfe32f4804daf51f
Dmitrii Podkorytov <podkorytov@mail.ru>
2023-02-27 14:27:30 -0600

    Add files via upload

---
+++ ListenAndSpeak.ps1
@@ -0,0 +1,111 @@
+#OBJECTIVE:
+# Learning for Listening, Reading and Repeating from file Input.txt. Actually it is a Book in TXT format.
+#
+#1) Text2Speach
+#
+#   https://thesysadminchannel.com/powershell-text-to-speech-how-to-guide/
+#
+#---------------------
+
+Function New-TextToSpeechMessage {
+<#
+.SYNOPSIS
+    This will use Powershell to have a message read out loud through your computer speakers.
+ 
+ 
+.NOTES
+    Name: New-TextToSpeechMessage
+    Author: theSysadminChannel
+    Version: 1.0
+    DateCreated: 2021-Feb-28
+ 
+.LINK
+    https://thesysadminchannel.com/powershell-text-to-speech-how-to-guide -
+ 
+.EXAMPLE
+    New-TextToSpeechMessage -Message 'This is the text I want to have read out loud' -Voice Zira
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Position = 0,
+            Mandatory = $true
+        )]
+ 
+        [string]    $Message,
+ 
+ 
+        [Parameter(
+            Position = 1,
+            Mandatory = $false
+        )]
+ 
+        [ValidateSet('David', 'Zira','Irina')]
+        [string]    $Voice = 'Zira'
+    )
+ 
+    BEGIN {
+        if (-not ([appdomain]::currentdomain.GetAssemblies() | Where-Object {$_.Location -eq 'C:\Windows\Microsoft.Net\assembly\GAC_MSIL\System.Speech\v4.0_4.0.0.0__31bf3856ad364e35\System.Speech.dll'})) {
+            Add-Type -AssemblyName System.Speech
+        }
+    }
+ 
+    PROCESS {
+        try {
+            $NewMessage = New-Object System.Speech.Synthesis.SpeechSynthesizer
+ 
+            if ($Voice -eq 'Zira') {
+                $NewMessage.SelectVoice("Microsoft Zira Desktop")
+            } else {
+                $NewMessage.SelectVoice("Microsoft " + $Voice + " Desktop")
+            }
+     
+            $NewMessage.Speak($Message)
+     
+        } catch {
+            Write-Error $_.Exception.Message
+        }        
+    }
+ 
+    END {}
+}
+
+Add-Type -AssemblyName System.Speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+Write-Output $speak.GetInstalledVoices().VoiceInfo
+
+New-TextToSpeechMessage -Message 'Engine Started' -Voice Zira
+
+#2) Read File with dot delimiter for sentencies 
+#
+#   https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-content?view=powershell-7.3
+#
+
+foreach($line in Get-Content .\Input.txt -Delimiter "." ) {
+# Work here
+ $starttime = Get-Date
+ Write-Output $line 
+ New-TextToSpeechMessage -Message $line -Voice David
+ $endtime = Get-Date
+
+ $tdiff = $endtime-$starttime
+ $delay = $tdiff.seconds + $tdiff.seconds
+
+ Write-Host "=====>> You have " $delay " seconds for repeat it , please read this sentence"
+
+ Start-Sleep -Seconds $delay
+   
+}
+
+
+#5) Fetch some from RSS feed 
+#
+# https://devblogs.microsoft.com/scripting/use-windows-powershell-to-parse-rss-feeds/
+#
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest?view=powershell-7.3
+#
+#
+#6) https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/start-sleep?view=powershell-7.3
+#
+#  Start-Sleep -Seconds 1.5
+#  
---
+++ ReadMe.md
@@ -0,0 +1,45 @@
+
+# OBJECTIVE:
+
+Program for learning English or another language in Listening Books
+and Repetition of the Text from some Sentence Book separately.
+
+# Requirements:
+ Windows PowerShell should be installed in Windows box.
+
+# How to use
+ Unzip archive to some folder.
+ 
+ Book format should be as plain TXT file.
+ Copy and Paste your's own text to file Input.txt
+ There is two ways for starting programm:
+ 1) This program is console application it can be started by clicking on 
+ file ListenAndSpeak.ps1
+
+ 2) Also this program can be started from Windows Console CMD.EXE
+    For doing that just start CMD.EXE process
+    Next start PowerShell
+    Next start ListenAndSpeak.ps1 file inside power shell
+
+E:\PowerShell\Listen_And_Speak>powershell.exe                                   Windows PowerShell
+(C) Корпорация Майкрософт (Microsoft Corporation). Все права защищены.
+
+PS E:\PowerShell\Listen_And_Speak> dir
+
+
+    Directory: E:\PowerShell\Listen_And_Speak
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a----         2/27/2023  11:51 AM            676 Announce
+-a----         2/27/2023   9:22 AM          31027 Input.txt
+-a----         2/27/2023  10:21 AM           3084 ListenAndSpeak.ps1
+-a----         2/27/2023   2:08 PM            703 ReadMe.md
+
+
+PS E:\PowerShell\Listen_And_Speak> .\ListenAndSpeak.ps1
+
+
+
+
